d9d1d2176a6f7c9d548148efe9c51ca76411bf04
joyceyskwon <joyceyskwon@gmail.com>
2020-06-05 21:57:08 -0400

    added action controller methods for token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  def token
+    request.headers.["Authorization"]
+  end
+
+  def encode_token
+    JWT.encode({user_id: user_id}, "ceed")
+  end
+
+  def decode_token
+    begin
+      JWT.decode(token, "ceed")[0]["user_id"]
+    rescue
+      nil
+    end
+  end
+
+  def curr_user
+    User.find_by(id: decode_token)
+  end
 end
