e8089bb03967a45f7872993f27931d1601c4fe66
joyceyskwon <joyceyskwon@gmail.com>
2020-06-05 21:22:45 -0400

    added posts controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create]
+      resources :posts, except: [:update]
 
       post '/login', to: 'auth#login'
       get '/current_user', to: 'auth#get_user_from_token'
