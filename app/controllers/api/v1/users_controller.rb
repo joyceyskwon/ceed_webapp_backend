46274ee95f7a8890b0c3e2775a2d6fdbe58f2775
joyceyskwon <joyceyskwon@gmail.com>
2020-06-05 21:50:40 -0400

    added bcrypt

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -22,6 +22,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :name)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
